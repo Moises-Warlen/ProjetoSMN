8101e8fe04c430983273232c0584f7b16cc0ad12
Moises-Warlen <moisesfatecads@gmail.com>
2024-05-03 10:12:11 -0300

    criando regras na controler de tarefa para listar as tarefas apenas do usuario logado

--- DesafioSMN.MVC/Controllers/TarefaController.cs
+++ DesafioSMN.MVC/Controllers/TarefaController.cs
@@ -1,26 +1,80 @@
-﻿using DesafioSMN.Infra.Repositorio;
+﻿using DesafioSMN.Dominio.Enums;
+using DesafioSMN.Infra.Data;
+using DesafioSMN.Infra.Repositorio;
 using DesafioSMN.MVC.Filters;
+using DesafioSMN.MVC.Helper;
 using DesafioSMN.MVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DesafioSMN.MVC.Controllers
 {
     [PaginaParafuncionarioLogado]
     public class TarefaController : Controller
     {
+
+        private readonly BancoContext _context;
+        private readonly ISessao _sessao;
+
+
+
+
         private readonly IFuncionarioRepositorio _funcionarioRepositorio;
 
         private readonly ITarefaRepositorio _tarefaRepositorio;
-        public TarefaController(ITarefaRepositorio tarefaRepositorio , IFuncionarioRepositorio funcionarioRepositorio)
+        public TarefaController(BancoContext context, ISessao sessao, ITarefaRepositorio tarefaRepositorio , IFuncionarioRepositorio funcionarioRepositorio)
         {
+
+            _context = context;
+            _sessao = sessao;
+
             _tarefaRepositorio = tarefaRepositorio;
             _funcionarioRepositorio = funcionarioRepositorio;
         }
         public IActionResult Index()
         {
 
-          List<TarefaModel> tarefas  = _tarefaRepositorio.BuscarTodos();
+
+
+            // Verifica se o funcionário está logado
+            var funcionarioLogado = _sessao.BuscarSessaoDoFuncionario();
+            if (funcionarioLogado == null)
+            {
+                // Redireciona para a página de login se o funcionário não estiver logado
+                return RedirectToAction("Index", "Login");
+            }
+
+
+
+
+            // Verifica se o funcionário não é um administrador
+            if (funcionarioLogado.Perfil != PerfilEmun.Admin)
+            {
+                // Obtém as tarefas do funcionário logado que não é um administrador
+                var tarefa = _context.Tarefas.Where(t => t.Responsavel == funcionarioLogado.Nome).ToList();
+
+                return View(tarefa);
+            }
+
+
+
+            // Se o funcionário for um administrador, exibe todas as tarefas
+            var todasTarefas = _context.Tarefas.ToList();
+            return View(todasTarefas);
+
+
+
+
+
+
+
+
+
+
+
+
+            List<TarefaModel> tarefas  = _tarefaRepositorio.BuscarTodos();
             return View(tarefas);
         }
         public IActionResult Criar()
